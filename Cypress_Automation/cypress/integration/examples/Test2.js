ed83a5e27fe4e122fc400f95484ba6fc8539aeaf
vinayagamurthi <vignesh5367@gmail.com>
2024-09-13 12:17:42 +0530

    Update Test2.js  a value is set 22

--- Cypress_Automation/cypress/integration/examples/Test2.js
+++ Cypress_Automation/cypress/integration/examples/Test2.js
@@ -2,3 +2,4 @@ console.log("hello world")
 console.log("new changes from vscode")
 console.log("again new changes happened from vscode")
 console.log("2 time again new changes happened from vscode")
+a = 22
