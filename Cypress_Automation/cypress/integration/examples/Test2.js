79af4ed10cae98d71c53789ed4528960f81c9987
vignesh2368 <vignesh.balakrishnan@atmecs.com>
2024-09-13 12:06:01 +0530

    test changes happen from vscode

--- Cypress_Automation/cypress/integration/examples/Test2.js
+++ Cypress_Automation/cypress/integration/examples/Test2.js
@@ -1,4 +1,4 @@
 console.log("hello world")
 console.log("new changes from vscode")
 console.log("again new changes happened from vscode")
-console.log("2 time again new changes happened from github")
+console.log("2 time again new changes happened from vscode")
